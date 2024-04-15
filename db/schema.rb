b048848c1ba69edf5dbd30ea542702999c64b06f
tzequi-og <146213960+tzequi-og@users.noreply.github.com>
2024-04-15 23:14:15 +0000

    scaffold books

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_04_05_213844) do
+ActiveRecord::Schema[7.0].define(version: 2024_04_15_231309) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "books", force: :cascade do |t|
+    t.string "title"
+    t.text "description"
+    t.boolean "released"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "movies", force: :cascade do |t|
     t.string "title"
