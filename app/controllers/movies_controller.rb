5534b7070463bc0e6c51c8cb3b29ca30b84d6d0b
tzequi-og <146213960+tzequi-og@users.noreply.github.com>
2024-04-15 23:11:59 +0000

    industrial grade

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -8,7 +8,7 @@ class MoviesController < ApplicationController
   end
 
   def show
-    the_id = params.fetch("path_id")
+    the_id = params.fetch("id")
 
     matching_movies = Movie.where({ :id => the_id })
 
@@ -32,7 +32,7 @@ class MoviesController < ApplicationController
   end
 
   def update
-    the_id = params.fetch("path_id")
+    the_id = params.fetch("id")
     the_movie = Movie.where({ :id => the_id })[0]
 
     the_movie.title = params.fetch("query_title")
@@ -48,7 +48,7 @@ class MoviesController < ApplicationController
   end
 
   def destroy
-    the_id = params.fetch("path_id")
+    the_id = params.fetch("id")
     the_movie = Movie.where({ :id => the_id })[0]
 
     the_movie.destroy
